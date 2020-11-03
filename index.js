0e058480f8903b5611c62f606198a8a94dffb0d4
ipek <curgulipek@gmail.com>
2020-11-03 21:57:07 +0300

    Files and folders created

--- index.js
+++ index.js
@@ -3,9 +3,8 @@
  */
 
 import 'react-native-gesture-handler';
-import Modal from 'react-native-modal';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import Router from './src/Router';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Router);
